44b11afcf87c7af95651e51e88dada336a203f1c
jason pitt <jnpitt@uw.edu>
2020-08-20 05:12:33 +0900

    updated UPDATE script, set scan time to 20 minutes

--- generate_coordinates.py
+++ generate_coordinates.py
@@ -1,10 +1,10 @@
 nums = [1,2,3,4,4,3,2,1,1,2,3,4]
 letters = ["A", "B", "C"]
-x_0 = 8000
-y_0 = 39000
+x_0 = 6800
+y_0 = 50000
 dx_well = 16500
 dy_well = 16500
-dx_plate = 83000
+dx_plate = 83500
 dy_plate = 56000
 
 with open("platecoordinates.dat", "w") as f:
